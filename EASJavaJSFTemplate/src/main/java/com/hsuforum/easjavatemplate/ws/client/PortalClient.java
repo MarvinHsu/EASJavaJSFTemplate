5141249b369c466feef0c407a2190788dc648343
Marvin <Marvin@WINDESKTOP1.hsuforum.com>
2020-04-19 12:14:44 +0800

    bug fix

--- EASJavaJSFTemplate/src/main/java/com/hsuforum/easjavatemplate/ws/client/PortalClient.java
+++ EASJavaJSFTemplate/src/main/java/com/hsuforum/easjavatemplate/ws/client/PortalClient.java
@@ -2,6 +2,7 @@ package com.hsuforum.easjavatemplate.ws.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
@@ -18,16 +19,21 @@ public class PortalClient {
 	private String portalWSURI;
 	
 	public UserWSVO findUserById(String systemCode, String account){
-
+		
 		Client client = ClientBuilder.newClient();
     	
     	WebTarget target= client.target(this.getPortalWSURI()).path("userResource");
     	
     	target=target.queryParam("systemCode", systemCode);
     	target=target.queryParam("account", account);
+    	
+    	Invocation.Builder builder = target
+    			.request(MediaType.APPLICATION_JSON)
+    			.accept(MediaType.APPLICATION_JSON)
+    			.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
     	UserWSVO userWSVO;
     	try{
-    		userWSVO = target.request(MediaType.APPLICATION_JSON).get(UserWSVO.class);
+    		userWSVO=builder.get(UserWSVO.class);
     	}catch(Exception e) {
     		e.printStackTrace();
     		userWSVO = null;
@@ -44,10 +50,13 @@ public class PortalClient {
     	WebTarget target= client.target(this.getPortalWSURI()).path("moduleResource");
     	
     	target=target.queryParam("systemCode", systemCode);
-    	
+    	Invocation.Builder builder = target
+    			.request(MediaType.APPLICATION_JSON)
+    			.accept(MediaType.APPLICATION_JSON)
+    			.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
     	ModuleWSVO2[] moduleWSVO2s;
     	try{
-    		moduleWSVO2s = target.request(MediaType.APPLICATION_JSON).get(ModuleWSVO2[].class);
+    		moduleWSVO2s = builder.get(ModuleWSVO2[].class);
     	}catch(Exception e) {
     		e.printStackTrace();
     		moduleWSVO2s = null;
